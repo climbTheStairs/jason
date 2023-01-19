c61e9ca8c8f9d597e38d90eb55a63e28c17c65bd
climbTheStairs <yxtqwf+github@gmail.com>
2023-01-19 01:32:37 -0800

    initial commit

---
+++ js/json2jason.js
@@ -0,0 +1,26 @@
+"use strict"
+const toJason = (k, v, omitKey) => {
+	k = JSON.stringify(k) + ": "
+	if (omitKey)
+		k = ""
+	if (typeof v === "object" && v && Object.entries(v).length) {
+		const isArr = Array.isArray(v)
+		const lbrace = isArr ? "[" : "{"
+		const rbrace = isArr ? "]" : "}"
+		const listTag = isArr ? "ol" : "ul"
+		return `<details open="open">` +
+			`<summary>${k}${lbrace}</summary>` +
+			`<${listTag}>` +
+			`<li>` +
+			Object.entries(v)
+				.map(([k, v]) => toJason(k, v, isArr))
+				.join(`,</li><li>`) +
+			`</li>` +
+			`</${listTag}>` +
+			`</details>${rbrace}`
+	}
+	return (k + JSON.stringify(v))
+		.replaceAll("&", "&amp;")
+		.replaceAll("<", "&lt;")
+		.replaceAll(">", "&gt;")
+}
